5ee1837ccdfd4b6818756ccc5a87de1417c76b6a
ABIJOSEPHGEORGE <abijosephgeorge@gmail.com>
2023-05-19 12:57:18 +0530

    added minor fix:

--- GetAndDecode.jsx
+++ GetAndDecode.jsx
@@ -1,6 +1,6 @@
 import jwtDecode from 'jwt-decode'
 
-function GetAndDecode({key}){
+function GetAndDecode(key){
     const keyItem = JSON.stringify(localStorage.getItem(key))
     return jwtDecode(keyItem)
 }
